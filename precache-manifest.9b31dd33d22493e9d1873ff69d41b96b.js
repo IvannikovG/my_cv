089bd0231ee579350a014380c685bc5b1ec6b3e6
Georgii Ivannikov <georgii@ki-elements.de>
2020-09-23 14:17:01 +0200

    Updates

---
+++ precache-manifest.9b31dd33d22493e9d1873ff69d41b96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dee69d3a87ed553795ddd65b4b6626",
+    "url": "/my_cv/index.html"
+  },
+  {
+    "revision": "f15be3ebbfee58d58755",
+    "url": "/my_cv/static/css/main.debbb51c.chunk.css"
+  },
+  {
+    "revision": "1aa3f33225ddbf528360",
+    "url": "/my_cv/static/js/2.16a7bfd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_cv/static/js/2.16a7bfd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15be3ebbfee58d58755",
+    "url": "/my_cv/static/js/main.5f95ea07.chunk.js"
+  },
+  {
+    "revision": "c3bd2bbe3f70329c5e65",
+    "url": "/my_cv/static/js/runtime-main.1c830f20.js"
+  },
+  {
+    "revision": "820a70663dcd9da3f49092467df20ca4",
+    "url": "/my_cv/static/media/my_photo3.820a7066.jpg"
+  }
+]);
